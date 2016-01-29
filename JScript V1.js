a724a9c9062ecd6f70dc4dcdd18f3e0f6bc7bfa8
iscotson <iscotson50@gmail.com>
2016-01-29 17:04:33 +1100

    Create JScript V1.js
    Sample JS file

---
+++ JScript V1.js
@@ -0,0 +1,60 @@
+// Passing object into functions
+// =============================
+
+// Our person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// We can make a function which takes persons as arguments
+// This one computes the difference in ages between two people
+var ageDifference = function(person1, person2) {
+    return person1.age - person2.age;
+}
+
+var alice = new Person("Alice", 30);
+var billy = new Person("Billy", 25);
+
+// get the difference in age between alice and billy using our function
+var diff = ageDifference(alice,billy)
+
+// Our person constructor
+function Person (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// We can make a function which takes persons as arguments
+// This one computes the difference in ages between two people
+var ageDifference = function(person1, person2) {
+    return person1.age - person2.age;
+};
+
+// Make a new function, olderAge, to return the age of
+// the older of two people
+var olderAge = function(person1, person2) {
+    if (person1.age > person2.age) {
+        return person1.age
+    }   else {
+        return person2.age;
+    }
+};   
+// Let's bring back alice and billy to test our new function
+var alice = new Person("Alice", 30);
+var billy = new Person("Billy", 25);
+
+console.log("The older person is " + olderAge(alice, billy));
+
+function Circle (radius) {
+    this.radius = radius;
+    this.area = function () {
+        return Math.PI * this.radius * this.radius;
+        
+    };
+    // define a perimeter method here
+    
+     this.perimeter = function() {
+        return 2 * Math.PI * this.radius;
+    };
+};
